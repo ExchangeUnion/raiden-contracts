f9d00fdc7fa1bee3b47a1622e6d8fc36c28fa9b3
Moshe Shababo <dicentralized@gmail.com>
2019-05-29 20:24:18 +0300

    SecretRegistry.sol to use sha256 instead of keccak256

--- raiden_contracts/contracts/SecretRegistry.sol
+++ raiden_contracts/contracts/SecretRegistry.sol
@@ -7,7 +7,7 @@ contract SecretRegistry {
 
     string constant public contract_version = "0.4.0";
 
-    // keccak256(secret) => block number at which the secret was revealed
+    // sha256(secret) => block number at which the secret was revealed
     mapping(bytes32 => uint256) private secrethash_to_block;
 
     event SecretRevealed(bytes32 indexed secrethash, bytes32 secret);
@@ -18,7 +18,7 @@ contract SecretRegistry {
     /// @return true if secret was registered, false if the secret was already
     /// registered.
     function registerSecret(bytes32 secret) public returns (bool) {
-        bytes32 secrethash = keccak256(abi.encodePacked(secret));
+        bytes32 secrethash = sha256(abi.encodePacked(secret));
         if (secret == bytes32(0x0) || secrethash_to_block[secrethash] > 0) {
             return false;
         }
@@ -42,7 +42,7 @@ contract SecretRegistry {
     }
 
     /// @notice Get the stored block number at which the secret was revealed.
-    /// @param secrethash The hash of the registered secret `keccak256(secret)`.
+    /// @param secrethash The hash of the registered secret `sha256(secret)`.
     /// @return The block number at which the secret was revealed.
     function getSecretRevealBlockHeight(bytes32 secrethash) public view returns (uint256) {
         return secrethash_to_block[secrethash];
